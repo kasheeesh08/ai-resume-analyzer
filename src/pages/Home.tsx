2769e2c0386e14dd44844f8972e11a45aeb6e73e
kasheeesh08 <kashishhome123@gmail.com>
2026-06-05 21:36:57 +0530

    Created initial landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
-    <h1>Home Page</h1>
-  )
+    <section>
+      <h1>Track Your Applications & Resume Ratings</h1>
+
+      <h2>Review your resume with AI-powered feedback</h2>
+
+      <p>
+        Upload your resume and receive ATS-style insights,
+        improvement suggestions, and job match feedback.
+      </p>
+
+      <Link to="/upload">
+        Analyze Resume
+      </Link>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
